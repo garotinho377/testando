0968e14ba8378b4f19e4d7d19b0326b3c793bc6d
garotinho 377 <danielsilvalindo3@gmail.com>
2021-03-08 14:57:08 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,19 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/994407645278 ou digite *${prefix}owner*_
+
+*NOTA*
+
+* 🐊🚩 :*l_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/554498179556*
+║
+╚═〘  🔥ツ𝐒4𝐀𝐍𝐕𝐈𝐂𖣘💫  〙
+`
+}
+exports.iklan = iklan
